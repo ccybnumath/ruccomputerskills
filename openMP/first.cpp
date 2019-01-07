fc7f79bd61f8dcdf3ac56e54af2d6416264809b0
chencanyi <24704141@qq.com>
2019-01-07 13:05:50 +0800

    add

---
+++ openMP/first.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+  int total = omp_get_max_threads();
+#pragma omp parallel
+{ // parallel region construct
+  int id = omp_get_thread_num();
+  printf("Greetings from process %d out of %d.\n", 
+         id, total);
+}
+printf("Parallel ends.\n");
+return 0;
+}
